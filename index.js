cd22aa81369acac8ce62f7dc9ecd9a40594593e3
samuel <samuelkelvinmwangi@gmail.com>
2023-04-01 18:21:14 +0300

    objects assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your solution in this file!
+function employees(_name){
+  this.name = 'Sam'
+
+  return employees;
+}
+
+function updateEmployeeWithKeyAndValue( name, street){
+  this.name = 'Sam',
+  this.street = '11 Broadway';
+
+}
